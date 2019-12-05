9ba2d370348379e17b4dbca9684e2a3245f6d821
santh0se <54751078+santh0se@users.noreply.github.com>
2019-12-04 21:51:41 -0600

    created socket structures

---
+++ Client/client.c
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "client.h"
+#include <sys/socket.h>                             /* these are socket-related */
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char* argv[])
+{
+    int client_sd;                                              /* sd stands for socket descriptor*/
+    int option = 1;
+    int bytes_read;
+    char read_buffer[READ_BUFFER_SIZE] = {0};                   /* this array is to read the data from client */
+    struct sockaddr_in server_address;
+    int addr_length = sizeof(server_address);
+    char * client_hello = "Hello, this is client!";
+    int done = 0;
+    
+    printf("Starting client.\n");
+    
+/* ----------------------------------------------------------------------------------------- */
+    /* Create Socket Structure ... */
+    client_sd = socket(AF_INET, SOCK_STREAM, 0);                 /* create socket structure */
+    if(client_sd == 0)                                           /* INET = internet socket */
+    {
+        printf("Could not create socket (client)!\n");
+        exit(0);
+    }
+    
+/* ------------------------------------------------------------------------------------------- */
+    
+    /* Construct Server Address (IP and PORT) in network bytes mode */
+    server_address.sin_family = AF_INET;                                            /* server address (internet) */
+    server_address.sin_port = htons(SERVER_PORT);
+    
+    if(inet_pton(AF_INET, LOCAL_SERVER_IP, &server_address.sin_addr)<=0)            /* use local address only for testing */
+    {
+        printf("Invalid server address! Could not convert into network bytes!\n");
+        exit(0);
+    }
+    
+/* -------------------------------------------------------------------------------------------- */
+    
+    /* Connecting to server ... */
+    printf("Connecting to server: %s at PORT: %d\n", LOCAL_SERVER_IP, SERVER_PORT);
+    int result = connect(client_sd, (struct sockaddr *) &server_address, sizeof(server_address));
+    if(result < 0)
+    {
+        printf("Connection to server failed!\n");
+        exit(0);
+    }
+    printf("Connection to server is successful!\n");
+    
+/* -------------------------------------------------------------------------------------------- */
+    
+    while(done == 0)
+    {
+        char line[1024];
+        char *buffer = line;
+        size_t buffer_size = 1024;
+        
+        printf("\n Please enter a line/message to send to the server: ");
+        if(getline(&buffer, &buffer_size, stdin) == -1)
+        {
+            done = 1;
+            printf("User has terminated! Exiting program! \n");
+        }
+        else
+        {
+            /* Start SENDING bytes to server */
+            printf("Sending user message to server\n");
+            send(client_sd, buffer, strlen(buffer), 0);
+            
+            /* Start READING bytes from server */
+            printf("Reading user message from server!\n");
+            bytes_read = read(client_sd, read_buffer, READ_BUFFER_SIZE);
+            printf("Bytes sent (echo) by server: %s\n", read_buffer);
+        }
+    }
+    
+    printf("Exiting client!\n");
+    
+}
---
+++ Client/client.h
@@ -0,0 +1,12 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+#include <stdio.h>
+#include <stdlib.h>
+#define SERVER_PORT 1025
+/*#define SERVER_PORT 80*/
+#define READ_BUFFER_SIZE 1024
+#define LOCAL_SERVER_IP "127.0.0.1"
+/*#define LOCAL_SERVER_IP "172.217.6.110" -- google.com */
+
+
+#endif
