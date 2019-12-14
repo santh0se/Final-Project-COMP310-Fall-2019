1636152a9748cde6731001592b44fe22c032bddf
santh0se <54751078+santh0se@users.noreply.github.com>
2019-12-13 21:39:30 -0600

    cleaning up client.h

--- Client/client.h
+++ Client/client.h
@@ -8,9 +8,9 @@
 #define LOCAL_SERVER_IP "127.0.0.1"
 /*#define LOCAL_SERVER_IP "172.217.6.110" -- google.com */
 
-/*--------------Methods API-------------------------*/
+/*-------------- API | GLOBAL METHODS-------------------------*/
+
 void display_weather_menu();
 void display_stock_menu();
-
 
 #endif
