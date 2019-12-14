017b088d3881c5f9dd0d96df366ca70edbc195e8
santh0se <54751078+santh0se@users.noreply.github.com>
2019-12-13 21:03:49 -0600

    included method APIs

--- Client/client.h
+++ Client/client.h
@@ -2,11 +2,15 @@
 #define CLIENT_H
 #include <stdio.h>
 #include <stdlib.h>
-#define SERVER_PORT 1025
+#define SERVER_PORT 1027
 /*#define SERVER_PORT 80*/
 #define READ_BUFFER_SIZE 1024
 #define LOCAL_SERVER_IP "127.0.0.1"
 /*#define LOCAL_SERVER_IP "172.217.6.110" -- google.com */
+
+/*--------------Methods API-------------------------*/
+void display_weather_menu();
+void display_stock_menu();
 
 
 #endif
